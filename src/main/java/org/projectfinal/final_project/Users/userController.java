8dda52f8295d1391dbfe03bca0b51faa8d0bc39f
mombe <h220462g@hit.ac.zw>
2024-03-19 20:33:44 +0200

    implemented endpoints of the user model

--- src/main/java/org/projectfinal/final_project/Users/userController.java
+++ src/main/java/org/projectfinal/final_project/Users/userController.java
@@ -1,4 +1,34 @@
 package org.projectfinal.final_project.Users;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+
+@RestController
 public class userController {
+
+    @Autowired
+    private final userService userService;
+
+    public userController(org.projectfinal.final_project.Users.userService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/upload-csv")
+    public ResponseEntity<String> uploadCsv(@RequestParam("file") MultipartFile file) {
+        try {
+            userService.saveDataFromCsv(file);
+            return ResponseEntity.ok("CSV file uploaded and data saved successfully.");
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error occurred while processing the CSV file.");
+        }
+    }
 }
--- src/main/java/org/projectfinal/final_project/Users/userService.java
+++ src/main/java/org/projectfinal/final_project/Users/userService.java
@@ -1,8 +1,17 @@
 package org.projectfinal.final_project.Users;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,12 +20,60 @@ public class userService {
 
     @Autowired(required = true)
     private final userRepo userRepo;
+    private PasswordEncoder passwordEncoder;
 
     public userService(org.projectfinal.final_project.Users.userRepo userRepo) {
         this.userRepo = userRepo;
     }
 
+
+    public void saveDataFromCsv(MultipartFile file) throws IOException {
+        try (Reader reader = new InputStreamReader(file.getInputStream())) {
+            CSVReader csvReader = new CSVReader(reader);
+            String[] nextRecord;
+
+            while ((nextRecord = csvReader.readNext()) != null) {
+                // Assuming that the first column contains the username and the second column contains the email
+                String hitmail = nextRecord[0];
+                String password = nextRecord[1];
+                int yearEnrolled = Integer.parseInt(nextRecord[2]); // Assuming nextRecord[2] is the yearEnrolled field
+                String firstName = nextRecord[3];
+                Status status = Status.valueOf(nextRecord[4]); // Assuming nextRecord[4] is the Status field
+                String lastName = nextRecord[5];
+                String programme = nextRecord[6];
+                String department = nextRecord[7];
+                String dateString = nextRecord[8];
+                Long phoneNumber = Long.parseLong(nextRecord[9]);
+                String email = nextRecord[10];
+                String nextOfKin = nextRecord[11];
+                String guardianNumber = nextRecord[12];
+                String address = nextRecord[13];
+
+                // Create a Usermodel entity and set the data
+                userModel user = new userModel();
+                user.setHitmail(hitmail);
+                user.setPassword(passwordEncoder.encode(user.getPassword()));
+                user.setYearEnrolled(yearEnrolled);
+                user.setFirstName(firstName);
+                user.setStatus(status);
+                user.setLastName(lastName);
+                user.setProgramme(programme);
+                user.setDepartment(department);
+                user.setDateOfBirth(Date.valueOf(dateString));
+                user.setPhoneNumber(phoneNumber);
+                user.setEmail(email);
+                user.setNextOfKin(nextOfKin);
+                user.setGuardianNumber(guardianNumber);
+                user.setAddress(address);
+                userRepo.save(user);
+            }
+        } catch (CsvValidationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public userModel saveUserModel(userModel UserModel){
+
         return userRepo.save(UserModel);
     }
     public List<userModel> getAllEUsers() {
